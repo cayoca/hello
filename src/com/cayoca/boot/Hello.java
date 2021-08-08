3f7eb30832bca12696612f12994e1319d15f4ae5
ray <cayoca@sina.cn>
2021-08-08 17:18:52 +0800

    create src&res

---
+++ src/com/cayoca/boot/Hello.java
@@ -0,0 +1,15 @@
+package com.cayoca.boot;
+
+public class Hello {
+	public static void main(String[] args) {
+		for(int i=0;i<5;i++) {
+			if(i==3) {
+				System.out.print(i*9);
+			}
+		}
+	}
+	static {
+		System.out.println("hello");
+	}
+
+}
---
+++ src/com/cayoca/boot/HelloClassLoader.java
@@ -0,0 +1,53 @@
+package com.cayoca.boot;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Base64;
+
+public class HelloClassLoader extends ClassLoader {
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException {
+
+		byte[] bytes = readClassFile("res/Hello.xlass");
+		trans(bytes);
+		
+		HelloClassLoader helloClassLoader = new HelloClassLoader();
+		Class<?> clz = helloClassLoader.defineClass("Hello",bytes,0,bytes.length);
+		clz.getMethod("hello").invoke(clz.newInstance());
+				
+	}
+	
+	private static void trans(byte[] bytes) {
+		for(int i=0;i<bytes.length;i++) {
+			bytes[i] = (byte) (255-bytes[i]);
+		}
+	}
+
+	
+	private static byte[] readClassFile(String path) {
+		File file = new File(path);
+		byte[] bytes = new byte[(int)file.length()];
+        InputStream in = null;
+        try {
+            in = new FileInputStream(file);
+            bytes = in.readAllBytes();
+            in.close();
+            return bytes;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e1) {
+                }
+            }
+        }
+	}
+	
+
+}
